2160b025737f295f91e629616ac3dd8f58bec320
Kelly Burgess <kellyburgess91@gmail.com>
2018-09-05 15:18:11 -0700

    working lolcat clock

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var time = new Date().getHours();
 var messageText;
 var noon = 12;
 var evening = 18; // 6PM
-var wakeupTime = 9; // 9AM
+var wakeUpTime = 9; // 9AM
 var lunchTime = 12; // 12PM
 var partyTime = 17; // 5PM
 var napTime = lunchTime + 2; // 2PM
@@ -18,7 +18,7 @@ image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/upl
 } else if (time == lunchTime) {
 	image="https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
     messageText = "IZ NOM NOM NOM TIME!!";
-} else if (time == wakeupTime) {
+} else if (time == wakeUpTime) {
 	image="https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
     messageText = "IZ TIME TO GETTUP.";
 } else if (time < noon) {
@@ -66,3 +66,46 @@ var updateClock=function(){
 };
 updateClock();
 setInterval(updateClock, 1000);
+var isPartyTime = false;
+var partyTimeButton=document.getElementById("partyTimeButton"); 
+var partyEvent = function() {
+   
+   if (isPartyTime === false) {
+      isPartyTime = true;
+      time = partyTime;
+	   // text in the button should read "Party Over"
+	  partyTimeButton.innerText="Party Over";
+      // color of the button should be "#0A8DAB" (bonus!)
+	   partyTimeButton.style.backgroundColor="#0A8DAB";
+	   
+   }
+   else {
+      isPartyTime = false;
+      time = new Date().getHours();
+      // text in the button should read "PARTY TIME!"
+	   partyTimeButton.innerText="Party Time!";
+      // color of the button should be "#222" (bonus!)
+	      partyTimeButton.style.backgroundColor="#222";
+   }
+};
+partyTimeButton.addEventListener("click", partyEvent);
+var
+lunchTimeSelector=document.getElementById("lunchTimeSelector");
+var wakeUpTimeSelector=document.getElementById("wakeUpTimeSelector");
+var napTimeSelector=document.getElementById("napTimeSelector");
+var lunchEvent = function() {
+    lunchTime = lunchTimeSelector.value;
+};
+ 
+var wakeUpEvent = function() {
+    wakeUpTime = wakeUpTimeSelector.value;
+};
+ 
+var napEvent = function() {
+	napTime = napTimeSelector.value;
+	return napTime;
+};
+lunchTimeSelector.addEventListener('change', lunchEvent);
+wakeUpTimeSelector.addEventListener("change", wakeUpEvent);
+napTimeSelector.addEventListener('change', napEvent);
+
